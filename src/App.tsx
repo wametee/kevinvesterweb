a9c7d4ddd4fe6c4d45773f420277b79c1024768b
Joseph Wamiti Mugo <jowamupro@DESKTOP-2MO1LN4.localdomain>
2025-01-17 03:43:08 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+
+      <h1>Welcome Abroad Kelvin</h1>
+      <p>Let us test your IQ Here</p>
+     
+     
+    </div>
+  )
+}
+
+export default App
